f0aa87590b3eb17dee9c2862c4242537dd366aac
Robbie Hanson <robbiehanson@deusty.com>
2020-04-16 21:58:05 -0700

    Bug fix: respondsToSelector won't work here, so switching to a workaround. Addresses issue #515
    # Conflicts:
    #	Testing/Swift-desktop/Swift-desktop/AppDelegate.swift

--- Testing/Swift-desktop/Swift-desktop/AppDelegate.swift
+++ Testing/Swift-desktop/Swift-desktop/AppDelegate.swift
@@ -6,8 +6,9 @@ import YapDatabase
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		self.testDatabase()
-//		self.testUpgrade()
+	//	testDatabase()
+	//	testUpgrade()
+		testIssue515()
 	}
 	
 	private func testDatabase() {
@@ -18,67 +19,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		let databaseURL = baseDir.appendingPathComponent("database.sqlite")
 
 		let database = YapDatabase(url: databaseURL)
-        database?.register(YapDatabaseRelationship(), withName: "relationship")
-        
-        database?.registerCodableSerialization(List.self, forCollection: kCollection_List)
-        database?.registerCodableSerialization(A.self, forCollection: "A")
-        database?.registerCodableSerialization(B.self, forCollection: "B")
+		database?.registerCodableSerialization(List.self, forCollection: kCollection_List)
 
-        let config = YapDatabaseConnectionConfig()
-        config.objectCacheEnabled = false
-        let databaseConnection = database?.newConnection(config)
-//		let uuid = "fobar"
-//
-//		databaseConnection?.asyncReadWrite({ (transaction) in
-//
-//			let list = List(uuid: uuid, title: "Groceries")
-//			transaction.setObject(list, forKey: list.uuid, inCollection: kCollection_List)
-//		})
-//
-//		databaseConnection?.asyncRead({ (transaction) in
-//
-//			if let list: List = transaction.object(forKey: uuid, inCollection: kCollection_List) as? List {
-//				print("Read list: \(list.title)")
-//			} else {
-//				print("wtf")
-//			}
-//
-//			transaction.iterateCollections { (collection, stop) in
-//
-//				print("row: collection: \(collection)")
-//			}
-//
-//			transaction.iterateKeys(inCollection: kCollection_List) { (key, stop) in
-//
-//				print("row: key: \(key)")
-//			}
-//
-//			transaction.iterateKeysAndObjects(inCollection: kCollection_List) { (key, list: List, stop) in
-//
-//				print("Iterate list: \(list.title)")
-//			}
-//		})
-        
-        let a = A(id: UUID(), name: "I'm a")
-        let b = B(id: UUID(), aID: a.id, text: "I'm b")
-        
-        databaseConnection?.readWrite({ tx in
-            tx.setObject(a, forKey: a.id.uuidString, inCollection: "A")
-            tx.setObject(b, forKey: b.id.uuidString, inCollection: "B")
-        })
-        
-        databaseConnection?.read({ tx in
-            guard let fetched = tx.object(forKey: a.id.uuidString, inCollection: "A") as? A else { return }
-            print("fetched A: \(fetched)")
-            
-            guard let vtx = tx.ext("relationship") as? YapDatabaseRelationshipTransaction else {
-                return
-            }
-            print("vtx: \(vtx)")
-            vtx.enumerateEdges(withName: "B->A") { (edge, stop) in
-                print("edge: \(edge)")
-            }
-        })
+		let databaseConnection = database?.newConnection()
+		let uuid = "fobar"
+
+		databaseConnection?.asyncReadWrite({ (transaction) in
+
+			let list = List(uuid: uuid, title: "Groceries")
+			transaction.setObject(list, forKey: list.uuid, inCollection: kCollection_List)
+		})
+
+		databaseConnection?.asyncRead({ (transaction) in
+
+			if let list: List = transaction.object(forKey: uuid, inCollection: kCollection_List) as? List {
+				print("Read list: \(list.title)")
+			} else {
+				print("wtf")
+			}
+
+			transaction.iterateCollections { (collection, stop) in
+
+				print("row: collection: \(collection)")
+			}
+
+			transaction.iterateKeys(inCollection: kCollection_List) { (key, stop) in
+
+				print("row: key: \(key)")
+			}
+
+			transaction.iterateKeysAndObjects(inCollection: kCollection_List) { (key, list: List, stop) in
+
+				print("Iterate list: \(list.title)")
+			}
+		})
 	}
 	
 	private func testUpgrade() {
@@ -107,6 +81,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 			else {
 				print("no foobar for you")
 			}
+		}
+	}
+	
+	private func testIssue515() {
+		
+		let baseDirs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+		let baseDir = baseDirs[0]
+
+		let databaseURL = baseDir.appendingPathComponent("database.sqlite")
+		
+		let database = YapDatabase(url: databaseURL)
+		
+		let collection = "issue515"
+		database?.registerCodableSerialization(Issue515.self, forCollection: collection)
+		
+		let ext = YapDatabaseRelationship()
+		database?.register(ext, withName: "relationships")
+		
+		let databaseConnection = database?.newConnection()
+		
+		databaseConnection?.asyncReadWrite {(transaction) in
+			
+			let test = Issue515(foobar: 42)
+			
+			transaction.setObject(test, forKey: "key", inCollection: collection)
 		}
 	}
 }
